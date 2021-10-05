a40a44e1dc31661b3a613ec4c9fd16ea6093865f
User <User@DESKTOP-2J1AS4M>
2021-10-05 12:33:05 +0700

    Add project files.

---
+++ FirstConsoleApp_006/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstConsoleApp_006
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
